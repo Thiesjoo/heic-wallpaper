0afa457fd00267c3bb31b88aed532f0194cf07d7
Thies Nieborg <git@thies.dev>
2022-07-04 22:48:12 +0200

    Added wallpaper fetching

--- api/app.py
+++ api/app.py
@@ -1,12 +1,10 @@
 from datetime import datetime
+import json
 import os
 from uuid import uuid4
-from flask import Flask, jsonify, request, url_for
+from flask import Flask, jsonify, redirect, request, url_for
 from config import AppConfig
 from workers.image_processor import func1, handle_image
-import heic
-import pyheif
-from PIL import Image
 from tasks import tasks
 
 ALLOWED_EXTENSIONS = {"heic", "png", "jpg", "jpeg", "gif"}
@@ -20,7 +18,10 @@ def allowed_file(filename):
     return "." in filename and get_extension(filename) in ALLOWED_EXTENSIONS
 
 
-app = Flask(__name__)
+app = Flask(
+    __name__,
+    static_folder="/static/",
+)
 app.config["UPLOAD_FOLDER"] = AppConfig.UPLOAD_FOLDER
 os.makedirs(os.path.join(AppConfig.UPLOAD_FOLDER), exist_ok=True)
 os.makedirs(os.path.join(AppConfig.PROCESSED_FOLDER), exist_ok=True)
@@ -66,44 +67,30 @@ def upload_new_wallpaper():
 @app.route("/wallpapers")
 def get_wallpapers():
     # Returns a list of all available .heic wallpapers with identifiers?
-    wallpapers = [
-        f.name
-        for f in os.scandir("wallpapers")
-        if f.is_file() and f.name.endswith(".heic")
-    ]
+    wallpapers = [f.name for f in os.scandir(f"{AppConfig.PROCESSED_FOLDER}")]
     return jsonify(wallpapers)
 
 
 @app.route("/wallpaper/<string:name>")
 def get_wallpaper(name: str):
-    starttime = datetime.now()
-    print(name)
     if not name.endswith(".heic"):
         # special logic?
         assert False
 
-    c = heic.get_wallpaper_config(f"wallpapers/{name}")
-    print(c)
-    # TODO: Do this check on upload
-    if "si" in c:
-        print("Sun based wallpapers are not yet supported.", file=sys.stderr)
-        sys.exit(1)
+    if not os.path.exists(f"{AppConfig.PROCESSED_FOLDER}/{name}"):
+        return "Image does not exist", 404
 
-    times = c["ti"]
-    times.sort(key=lambda x: x["t"])
-    prev = -0.1
-    for d in times:
-        cur = float(d["t"])
-        if not 0.0 <= cur <= 1.0:
-            print("Warning: Invalid time specification found. Might skip some images.")
-        if cur == prev:
-            print("Warning: Ambigous time specifiation found. Might skip some images.")
-        prev = cur
+    json_location = f"{AppConfig.PROCESSED_FOLDER}/{name}/data.json"
+    with open(json_location, "r") as f:
+        data = json.load(f)
 
-    times = c["ti"]
-    times.sort(key=lambda x: x["t"])
-    now = datetime.now().time()
-    nowsecs = now.hour * 60 * 60 + now.minute * 60 + now.second
+        times = data["data"]
+
+    nowsecs = request.args.get("time", type=int)
+    if not nowsecs:
+        now = datetime.now().time()
+        nowsecs = now.hour * 60 * 60 + now.minute * 60 + now.second
+
     last_one = times[-1]
     for time in times:
         if float(time["t"]) * 60 * 60 * 24 > nowsecs:
@@ -115,28 +102,7 @@ def get_wallpaper(name: str):
         last_one = time
     index = last_one["i"]
 
-    heif_container = pyheif.open_container(f"wallpapers/{name}")
-    all_images = heif_container.top_level_images
+    return redirect(url_for("static", filename=f"processed/{name}/{index}.png"))
 
-    assert index < len(all_images)
 
-    heif_file = all_images[index].image
-
-    heif_file.load()
-    print(heif_file)
-    img = Image.frombytes(
-        heif_file.mode,
-        heif_file.size,
-        heif_file.data,
-        "raw",
-        heif_file.mode,
-        heif_file.stride,
-    )
-
-    img.thumbnail((3000, 3000))
-    img.save("test.png", quality=85, optimize=True)
-
-    # print()
-
-    return jsonify(index, (datetime.now() - starttime).microseconds // 1000)
-    # Returns a URL (which nginx? should serve) for a specific name and a specific time
+# Returns a URL (which nginx? should serve) for a specific name and a specific time
