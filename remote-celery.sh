a09b0e1b7a7c273d10a1222e43e4b919082d9c5e
Thies Nieborg <git@thies.dev>
2022-07-05 21:38:26 +0200

    Raspberrypi changes

--- remote-celery.sh
+++ remote-celery.sh
@@ -1,3 +1,3 @@
 sshfs -o sftp_server='/usr/bin/sudo /usr/lib/openssh/sftp-server',allow_other  thies@raspberrypi.local:/home/thies/prod/heic-wallpaper/static remotestatic/
-docker-compose -f docker-compose-remote-raspberry.yml up --build
+docker-compose -f remote-worker.yml up --build
 sudo umount remotestatic/
