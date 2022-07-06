94b59212fbbdbeb7c2c9bfbe3b142e80e0cab393
Thies Nieborg <git@thies.dev>
2022-07-06 14:14:32 +0200

    Detachted on prod

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 if [ "$(uname -m)" = "aarch64" ]; then
-    docker-compose -f docker-compose.yml -f docker-compose.arm.yml up --build
+    docker-compose -f docker-compose.yml -f docker-compose.arm.yml up --build -d
 else
     docker-compose up --build
 fi
